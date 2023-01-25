b51c07f5871d31d0a99916f4fa75c7c9c115217d
GitHub Action <action@github.com>
2023-01-25 07:18:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/wleese/intellijkpttree/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wleese/intellijkpttree/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wleese.intellijkpttree.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wleese.intellijkpttree.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
